57ca9159763d639e6a049a9ac622d1997e35c7ff
Gui <guidiegofreitas@yahoo.com.br>
2022-09-22 00:23:43 -0300

    up

---
+++ CalcularMenorNota.java
@@ -0,0 +1,29 @@
+package javaapplication5;
+import java.util.Scanner;
+
+public class CalcularMenorNota {
+
+    public static void main(String[] args) {
+        int nota1, nota2, nota3;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite a nota numero 1");
+        nota1 = sc.nextInt();
+        System.out.println("Digite a nota numero 2");
+        nota2 = sc.nextInt();
+        System.out.println("Digite a nota numero 3");
+        nota3 = sc.nextInt();
+        if (nota1 < nota2 && nota1 < nota3){
+            System.out.println("a menor nota e " + nota1);
+        }else {
+            if(nota2 < nota1 && nota2 < nota3){
+                System.out.println("a menor nota e " + nota2);
+            }else{
+                System.out.println("a menor nota e :" + nota3);
+            }
+            
+        }
+     
+    }
+    
+  }
+
